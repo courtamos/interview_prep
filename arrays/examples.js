2161d04ff0cd2690be44f5b152f2088a41dc3957
Courtney Amos <c.amos@shaw.ca>
2021-07-13 16:17:43 -0700

    implementing an array using classes

--- arrays/examples.js
+++ arrays/examples.js
@@ -19,3 +19,54 @@ console.log(strings);
 // splice
 strings.splice(2, 0, "alien"); // O(n/2) => O(n)
 console.log(strings);
+
+// implementing an array
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArr = new MyArray();
+newArr.push("hi");
+newArr.push("you");
+newArr.push("!");
+console.log(newArr);
+
+newArr.pop();
+console.log(newArr);
+
+newArr.delete(1);
+console.log(newArr);
