850bec10d93b459cc317b51319bd3d766df3f98c
Courtney Amos <c.amos@shaw.ca>
2021-07-20 14:15:39 -0700

    create a queue using linked lists

--- stacks_queues/examples.js
+++ stacks_queues/examples.js
@@ -90,3 +90,59 @@ console.log(myStack2.peek());
 myStack2.pop();
 myStack2.pop();
 console.log(myStack2);
+
+// queue: using linked lists
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+  }
+}
+
+const myQueue = new Queue();
+
+myQueue.enqueue("Joy");
+myQueue.enqueue("Matt");
+myQueue.enqueue("Pavel");
+myQueue.enqueue("Samir");
+console.log(myQueue);
+
+console.log(myQueue.peek());
+
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue);
