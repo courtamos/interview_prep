3b6fdb2ae79679a47c0ce5fb85ff01fd1d7b574d
Courtney Amos <c.amos@shaw.ca>
2021-07-22 13:25:26 -0700

    implement graph add node and add edge

--- graphs/examples.js
+++ graphs/examples.js
@@ -11,12 +11,72 @@ const edgeList = [
 ];
 
 // adjacent list - the index is the node and the value/s are the nodes neighbours
+// can be object or array
 const adjacentList = [[2], [2, 3], [0, 1, 3], [1, 2]];
 
 // adjacent matrix - use 0 and 1 to indicate is the node x has a connection node y (0 = NO, 1 = YES)
+// can be object or array
 const adjacentMatrix = [
   [0, 0, 1, 0], // 0 is connected to 2
   [0, 0, 1, 1], // 1 is connected to 2 && 3
   [1, 1, 0, 1], // 2 is connected to 0 && 1 && 3
   [0, 1, 1, 0], // 3 is connected to 1 && 2
 ];
+
+// graph example
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+
+  addEdge(node1, node2) {
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+  }
+
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex("0");
+myGraph.addVertex("1");
+myGraph.addVertex("2");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addVertex("6");
+myGraph.addEdge("3", "1");
+myGraph.addEdge("3", "4");
+myGraph.addEdge("4", "2");
+myGraph.addEdge("4", "5");
+myGraph.addEdge("1", "2");
+myGraph.addEdge("1", "0");
+myGraph.addEdge("0", "2");
+myGraph.addEdge("6", "5");
+
+myGraph.showConnections();
+//Answer:
+// 0-->1 2
+// 1-->3 2 0
+// 2-->4 1 0
+// 3-->1 4
+// 4-->3 2 5
+// 5-->4 6
+// 6-->5
